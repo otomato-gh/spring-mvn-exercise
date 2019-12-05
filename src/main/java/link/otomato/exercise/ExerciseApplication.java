29469db5afcef0ff1f96bc6588b839ad67904928
Anton Weiss <anton@otomato.link>
2019-12-05 14:32:57 +0200

    Package application to War instead of Jar

--- src/main/java/link/otomato/exercise/ExerciseApplication.java
+++ src/main/java/link/otomato/exercise/ExerciseApplication.java
@@ -2,11 +2,20 @@ package link.otomato.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class ExerciseApplication {
+public class ExerciseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	return application.sources(ExerciseApplication.class);
+    }
 }
+
